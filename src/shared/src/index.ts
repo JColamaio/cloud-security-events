e6ae461c2bb9d96fa2074ec421738e0816743f77
JColamaio <jonathan_colamaio@solutionbox.com.ar>
2026-01-28 12:48:07 +0000

    feat: export sqlite store from shared

--- src/shared/src/index.ts
+++ src/shared/src/index.ts
@@ -3,3 +3,4 @@ export * from "./schemas/event.schema.js";
 export type * from "./interfaces/queue.js";
 export type * from "./interfaces/store.js";
 export * from "./adapters/pubsub-emulator.js";
+export * from "./adapters/sqlite-store.js";
